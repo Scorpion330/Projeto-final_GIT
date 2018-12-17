3f6c4985326b2f951b5c0b14cbf543ce43ad7c63
laziAna <lazimoi@hotmail.com>
2018-12-17 20:11:52 +0000

    Contactos completos

--- project/src/main/java/pt/projetofinal/project/model/Menu.java
+++ project/src/main/java/pt/projetofinal/project/model/Menu.java
@@ -3,7 +3,7 @@ package pt.projetofinal.project.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection="Menu")
+@Document(collection="menu")
 public class Menu{
 	
 	@Id
